8394b6400f471422491ae7aa493d7942c577e5de
Kardian <kuszawet@gmail.com>
2022-09-28 15:40:33 +0200

    change Search Component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { useGlobalContext } from "../context";
+
 const Search = () => {
     return (
-        <h1>Search Component</h1>
+
+        <header className="search-container">
+            <form>
+                <input type="text" placeholder='type favorite meal' className='form-input' />
+                <button type="submit" className='btn'>search</button>
+                <button type="button" className='btn btn-hipster'>suprice me!</button>
+            </form>
+        </header>
+    
     )
 }
 
